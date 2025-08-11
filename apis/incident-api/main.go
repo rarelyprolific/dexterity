8ce53a2d5da87fe65785238c0d8a0441e9c3c1f3
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2025-08-11 20:05:35 +0100

    Stubbing out APIs

--- apis/incident-api/main.go
+++ apis/incident-api/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Welcome to the Dexterity Incident API")
+
+	router := gin.Default()
+	router.GET("/incidents", getIncidents)
+
+	router.Run("localhost:8900")
+}
+
+type incident struct {
+	ID    int    `json:"id"`
+	Title string `json:"summary"`
+}
+
+var incidents = []incident{
+	{ID: 1, Title: "The website has crashed"},
+	{ID: 2, Title: "User cannot log in"},
+	{ID: 3, Title: "There is an intermittent crash in a back end service"},
+	{ID: 4, Title: "A service won't start up due to an error"},
+	{ID: 5, Title: "A service runs out of memory shortly after starting up"},
+}
+
+func getIncidents(c *gin.Context) {
+	c.JSON(http.StatusOK, incidents)
 }
--- apis/question-api/main.go
+++ apis/question-api/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Welcome to the Dexterity Question API")
+
+	router := gin.Default()
+	router.GET("/questions", getQuestions)
+
+	router.Run("localhost:8910")
+}
+
+type question struct {
+	ID           int    `json:"id"`
+	QuestionText string `json:"question"`
+}
+
+var questions = []question{
+	{ID: 1, QuestionText: "How does this work?"},
+	{ID: 2, QuestionText: "What should be done here?"},
+	{ID: 3, QuestionText: "Who needs to know about this?"},
+	{ID: 4, QuestionText: "What does this UI need to look like?"},
+	{ID: 5, QuestionText: "How does the user authenticate?"},
+}
+
+func getQuestions(c *gin.Context) {
+	c.JSON(http.StatusOK, questions)
 }
