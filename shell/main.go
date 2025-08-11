121a9469c73edb2f347ac4a8cfe242dbfac13310
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2025-08-11 20:50:15 +0100

    Added example HTTP call to shell exe

--- shell/main.go
+++ shell/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
 
 func main() {
 	fmt.Println("Welcome to Dexterity Shell!")
+
+	// Example HTTP call
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://www.rarelyprolific.co.uk", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	rawHTML := string(bodyBytes)
+
+	fmt.Println("HTML HTTP Response Body:", rawHTML)
 }
